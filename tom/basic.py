8ca796572c33ec33ab54c71a0739f0d8574a2c3c
Tom <tom@contextfree.info>
2021-10-04 06:31:39 -0700

    Maybe type things

--- tom/basic.py
+++ tom/basic.py
@@ -1,17 +1,22 @@
 import jax
-import jax.numpy as jnp
+import jax.numpy as np
+# import numpy as np
+import typing as typ
+
+
+ArrayLike = typ.Union[float, np.ndarray]
 
 
 # TODO Benchmark with and without jit. (And cpu vs gpu.)
 @jax.jit
-def quad(x):
+def quad(x: ArrayLike) -> ArrayLike:
     return x ** 2
 
 
 def main():
     quad_grad = jax.grad(lambda x: quad(x).sum())
-    x = jnp.linspace(-4, 4, 17)
-    y = jnp.vstack([x, quad(x), quad_grad(x)])
+    x = np.linspace(-4, 4, 17)
+    y = np.vstack([x, quad(x), quad_grad(x)])
     print(y.T)
 
 
