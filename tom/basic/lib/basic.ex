19e843fc6351e5c7fc1e7a2873aaf0be90b7e7d8
Tom <tom@contextfree.info>
2021-10-05 06:30:32 -0700

    Maybe get exla working

--- tom/basic/lib/basic.ex
+++ tom/basic/lib/basic.ex
@@ -17,8 +17,21 @@ defmodule Basic do
     :world
   end
 
+  # See:
+  # https://github.com/elixir-nx/nx/blob/main/exla/bench/softmax.exs
+  # https://github.com/elixir-nx/nx/tree/main/nx#readme
+
   # @defn_compiler {EXLA, client: :host}
-  defn softmax(t) do
-    Nx.exp(t) / Nx.sum(Nx.exp(t))
+  defn softmax(x) do
+    Nx.exp(x) / Nx.sum(Nx.exp(x))
   end
+
+  @defn_compiler EXLA
+  defn host(x), do: softmax(x)
+
+  @defn_compiler {EXLA, client: :cuda}
+  defn cuda(x), do: softmax(x)
+
+  @defn_compiler {EXLA, client: :cuda, run_options: [keep_on_device: true]}
+  defn cuda_keep(x), do: softmax(x)
 end
