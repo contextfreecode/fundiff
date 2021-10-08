3fa9b0f45bf2228dac2219e6291cc8635c39d524
Tom <tom@contextfree.info>
2021-10-07 20:10:43 -0700

    Calculate nx gradient of linspace

--- tom/basic/basic.exs
+++ tom/basic/basic.exs
@@ -1,1 +1,16 @@
-IO.inspect(Nx.tensor([-1, 1]) |> Basic.quad_grad)
+defmodule Blah do
+
+  import Nx.Defn
+
+  defn calc_grad do
+    x = linspace(-4, 4, 17)
+    Nx.transpose(Nx.stack([x, x |> Basic.quad, x |> Basic.quad_grad]))
+  end
+
+  defn linspace(min, max, count) do
+    Nx.iota({count}) * (max - min) / (count - 1) + min
+  end
+
+end
+
+IO.inspect Blah.calc_grad
