9143b3de97bb760df55e3864a4455698773272f8
Tom <tom@contextfree.info>
2021-10-09 23:03:19 -0700

    Calcualte in different ways and jacobian

--- tom/invkin.py
+++ tom/invkin.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
 import typing as typ
 
 
@@ -11,8 +12,12 @@ class Link(typ.NamedTuple):
 Arm = list[Link]
 
 
+def armify(angles: jnp.ndarray, lengths: jnp.ndarray) -> jnp.ndarray:
+    return [Link(angle=angle, length=length) for angle, length in zip(angles, lengths)]
+
+
 @jax.jit
-def forward(*, arm: Arm) -> jnp.ndarray:
+def forward0(*, arm: Arm) -> jnp.ndarray:
     x = jnp.zeros(2)
     angle = 0.0
     for link in arm:
@@ -21,14 +26,41 @@ def forward(*, arm: Arm) -> jnp.ndarray:
     return x
 
 
+@jax.jit
+def forward1(*, angles: jnp.ndarray, lengths: jnp.ndarray) -> jnp.ndarray:
+    x = jnp.zeros(2)
+    angle = 0.0
+    for link_angle, link_length in zip(angles, lengths):
+        angle += link_angle
+        x += jnp.array([jnp.cos(angle), jnp.sin(angle)]) * link_length
+    return x
+
+
+@jax.jit
+def forward2(*, angles: jnp.ndarray, lengths: jnp.ndarray) -> jnp.ndarray:
+    angles = jnp.cumsum(angles)
+    offsets = jnp.stack([jnp.cos(angles), jnp.sin(angles)]) * lengths
+    return offsets.sum(axis=1)
+
+
 def main():
-    arm = [
-        Link(angle=jnp.pi / 2, length=1.0),
-        Link(angle=-jnp.pi / 4, length=1.0),
-        Link(angle=-jnp.pi / 4, length=0.5),
-    ]
-    x = forward(arm=arm)
-    print(x)
+    angles = jnp.array([0.5, -0.25, -0.25]) * jnp.pi
+    lengths = jnp.array([1.0, 1.0, 0.5])
+    print(forward0(arm=armify(angles=angles, lengths=lengths)))
+    print(forward1(angles=angles, lengths=lengths))
+    print(forward2(angles=angles, lengths=lengths))
+    forward0a = lambda angles: forward0(arm=armify(angles=angles, lengths=lengths))
+    arm_jac0 = jax.jacfwd(forward0a)
+    print(arm_jac0(angles))
+    forward1a = lambda angles: forward1(angles=angles, lengths=lengths)
+    arm_jac1 = jax.jacfwd(forward1a)
+    print(arm_jac1(angles))
+    forward2a = lambda angles: forward2(angles=angles, lengths=lengths)
+    arm_jac2 = jax.jacfwd(forward2a)
+    print(arm_jac2(angles))
+    print(np.linalg.pinv(arm_jac2(angles)))
+    # Was hanging on jnp.linalg.pinv, now crashing,
+    # print(jnp.linalg.pinv(arm_jac2(angles)))
 
 
 if __name__ == "__main__":
