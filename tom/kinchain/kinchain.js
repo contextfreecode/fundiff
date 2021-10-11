8d09463ad279e65c2148d9ddee00234611b8e970
Tom <tom@contextfree.info>
2021-10-11 08:02:03 -0700

    Render last frame

--- tom/kinchain/kinchain.js
+++ tom/kinchain/kinchain.js
@@ -20,30 +20,41 @@ function forward({ angles , lengths  }) {
 }
 function main1() {
     const seq = parseSeq();
+    console.log(seq);
     const chain = forward({
-        angles: seq.angleFrames[0],
+        angles: seq.angleFrames.slice(-1)[0],
         lengths: seq.lengths
     });
     console.log(chain);
-    render();
+    render(chain);
 }
 function parseSeq() {
     const field = document.getElementById("field");
     const lines = field.value.split("\n");
-    const rows = lines.map((line)=>{
+    let rows = lines.map((line)=>{
         const texts = line.replaceAll(/[^-+\d\.e]+/g, " ").trim().split(/\s+/);
         const vals = texts.map((text)=>parseFloat(text)
         );
         return vals;
     });
+    rows = rows.filter((row)=>row.length == rows[0].length
+    );
     return {
         angleFrames: rows.slice(1),
         lengths: rows[0]
     };
 }
-function render() {
+function render(chain) {
     const path = document.getElementById("path");
-    console.log(path);
+    const coords = chain.flatMap(({ point: { x , y  }  })=>[
+            x,
+            y
+        ]
+    );
+    console.log(coords);
+    const d = `M 0 0 L ${coords.join(" ")}`;
+    console.log(d);
+    path.setAttribute("d", d);
 }
 main1();
 export { main1 as main };
--- tom/kinchain/kinchain.ts
+++ tom/kinchain/kinchain.ts
@@ -25,25 +25,34 @@ function forward({ angles, lengths }: Arm): Transform[] {
 
 export function main() {
   const seq = parseSeq();
-  const chain = forward({ angles: seq.angleFrames[0], lengths: seq.lengths });
+  console.log(seq);
+  const chain = forward({
+    angles: seq.angleFrames.slice(-1)[0],
+    lengths: seq.lengths,
+  });
   console.log(chain);
-  render();
+  render(chain);
 }
 
 function parseSeq(): Seq {
   const field = document.getElementById("field") as HTMLTextAreaElement;
   const lines = field.value.split("\n") as string[];
-  const rows = lines.map((line) => {
+  let rows = lines.map((line) => {
     const texts = line.replaceAll(/[^-+\d\.e]+/g, " ").trim().split(/\s+/);
     const vals = texts.map((text) => parseFloat(text));
     return vals;
-  })
-  return {angleFrames: rows.slice(1), lengths: rows[0]};
+  });
+  rows = rows.filter((row) => row.length == rows[0].length);
+  return { angleFrames: rows.slice(1), lengths: rows[0] };
 }
 
-function render() {
-  const path = document.getElementById("path");
-  console.log(path);
+function render(chain: Transform[]) {
+  const path = document.getElementById("path")!;
+  const coords = chain.flatMap(({ point: { x, y } }) => [x, y]);
+  console.log(coords);
+  const d = `M 0 0 L ${coords.join(" ")}`;
+  console.log(d);
+  path.setAttribute("d", d);
 }
 
 main();
