b3b94c54a89601b6ca3b1c04b42019ba9d08e751
Tom <tom@contextfree.info>
2021-10-12 07:31:23 -0700

    Still fail

--- tom/nx/invkin.exs
+++ tom/nx/invkin.exs
@@ -8,19 +8,24 @@ defmodule InvKin do
   #   return offsets.sum(axis=1)
 
   defn forward(lengths, angles) do
+    # Nx.stack([lengths, angles])
     Nx.reduce(
-      Nx.iota(lengths.shape),
-      Nx.tensor([0.0, 0.0]),
-      fn i, x ->
-        x + Nx.tensor([Nx.cos(angles[i]), Nx.sin(angles[i])]) * lengths[i]
+      Nx.stack([lengths, angles]),
+      0.0,
+      [axes: [1]],
+      fn length_angle, x ->
+        # angle = length_angle[{0, 1}]
+        # length = length_angle[0]
+        # Nx.add(x, Nx.tensor([Nx.cos(angle), Nx.sin(angle)]) * length)
+        Nx.add(length_angle, x)
       end
     )
-    angles
   end
 
   defn invert(lengths, angles, goal) do
+    # Nx.stack([lengths, angles])
     forward(lengths, angles)
-    Nx.tensor(1)
+    # angles
   end
 
   def main do
@@ -28,7 +33,8 @@ defmodule InvKin do
     angles = Nx.tensor([0.5 * pi, -0.25 * pi, -0.25 * pi])
     lengths = Nx.tensor([1.0, 1.0, 0.5])
     goal = Nx.tensor([0.0, 1.0])
-    invert(lengths, angles, goal)
+    x = invert(lengths, angles, goal)
+    IO.inspect(x)
   end
 
 end
